edd829c147d8c700da86369a0040d322c9101861
calvincodes <arpitjain1821@gmail.com>
2019-11-08 13:11:56 -0600

    Trying to import category_wise_reviews from python file in same folder

--- myapp/main.py
+++ myapp/main.py
@@ -47,59 +47,60 @@ from bokeh.application import Application
 
 from bokeh.plotting import figure, curdoc
 
+from .category_wise_reviews import category_wise_reviews_tab
 
-def category_wise_reviews_tab(cd_and_vinyl, cd_and_vinyl_meta):
+# def category_wise_reviews_tab(cd_and_vinyl, cd_and_vinyl_meta):
 
-    # cd_and_vinyl = pd.read_csv('../dataset/CDs_and_Vinyl_5.csv', skipinitialspace=True)
-    # cd_and_vinyl_meta = pd.read_csv('../dataset/CDs_And_Vinyl_meta_5.csv', skipinitialspace=True)
-    # cd_and_vinyl = pd.read_csv(join(dirname(__file__), 'dataset', 'CDs_and_Vinyl_5.csv'), skipinitialspace=True)
-    # cd_and_vinyl_meta = pd.read_csv(join(dirname(__file__), 'dataset', 'CDs_And_Vinyl_meta_5.csv'), skipinitialspace=True)
+#     # cd_and_vinyl = pd.read_csv('../dataset/CDs_and_Vinyl_5.csv', skipinitialspace=True)
+#     # cd_and_vinyl_meta = pd.read_csv('../dataset/CDs_And_Vinyl_meta_5.csv', skipinitialspace=True)
+#     # cd_and_vinyl = pd.read_csv(join(dirname(__file__), 'dataset', 'CDs_and_Vinyl_5.csv'), skipinitialspace=True)
+#     # cd_and_vinyl_meta = pd.read_csv(join(dirname(__file__), 'dataset', 'CDs_And_Vinyl_meta_5.csv'), skipinitialspace=True)
 
-    combined_data = cd_and_vinyl.set_index('asin').join(cd_and_vinyl_meta.set_index('Product ID'))
-    category_to_avg_rating_list = combined_data.groupby('Category', as_index=False)['overall'].mean()
+#     combined_data = cd_and_vinyl.set_index('asin').join(cd_and_vinyl_meta.set_index('Product ID'))
+#     category_to_avg_rating_list = combined_data.groupby('Category', as_index=False)['overall'].mean()
 
-    def make_dataset(category_list):
-        return category_to_avg_rating_list[category_to_avg_rating_list['Category'].isin(category_list)]
+#     def make_dataset(category_list):
+#         return category_to_avg_rating_list[category_to_avg_rating_list['Category'].isin(category_list)]
 
-    category_selection = CheckboxGroup(labels=category_to_avg_rating_list.Category.unique().tolist(),
-                                       active=[0, 1])
-    initial_categories = [category_selection.labels[i] for i in category_selection.active]
-    src = make_dataset(initial_categories)
+#     category_selection = CheckboxGroup(labels=category_to_avg_rating_list.Category.unique().tolist(),
+#                                        active=[0, 1])
+#     initial_categories = [category_selection.labels[i] for i in category_selection.active]
+#     src = make_dataset(initial_categories)
 
-    p = figure(x_range=initial_categories, y_range=(1,6), plot_height=700, plot_width = 1000, title="Category Wise Reviews")
-    p.xgrid.grid_line_color = None
-    p.y_range.start = 0
+#     p = figure(x_range=initial_categories, y_range=(1,6), plot_height=700, plot_width = 1000, title="Category Wise Reviews")
+#     p.xgrid.grid_line_color = None
+#     p.y_range.start = 0
 
-    r = p.vbar(x=initial_categories, top=src.overall.tolist(), width=0.1)
-    ds = r.data_source
+#     r = p.vbar(x=initial_categories, top=src.overall.tolist(), width=0.1)
+#     ds = r.data_source
 
-    def update(attr, old, new):
+#     def update(attr, old, new):
 
-        # Get the list of carriers for the graph
-        categories_to_plot = [category_selection.labels[i] for i in
-                            category_selection.active]
-        # Make a new dataset based on the selected carriers and the
-        # make_dataset function defined earlier
-        new_src = make_dataset(categories_to_plot)
-        # Update the source used in the quad glpyhs
-        # src.data.update(new_src.data)
-        new_data = dict()
-        new_data['x_range'] = new_src.Category.unique().tolist()
-        new_data['x'] = new_src.Category.unique().tolist()
-        new_data['top'] = new_src.overall.tolist()
+#         # Get the list of carriers for the graph
+#         categories_to_plot = [category_selection.labels[i] for i in
+#                             category_selection.active]
+#         # Make a new dataset based on the selected carriers and the
+#         # make_dataset function defined earlier
+#         new_src = make_dataset(categories_to_plot)
+#         # Update the source used in the quad glpyhs
+#         # src.data.update(new_src.data)
+#         new_data = dict()
+#         new_data['x_range'] = new_src.Category.unique().tolist()
+#         new_data['x'] = new_src.Category.unique().tolist()
+#         new_data['top'] = new_src.overall.tolist()
 
-        p.x_range.factors = new_data['x_range']
+#         p.x_range.factors = new_data['x_range']
 
-        ds.data = new_data
+#         ds.data = new_data
 
-    category_selection.on_change('active', update)
-    controls = WidgetBox(category_selection)
+#     category_selection.on_change('active', update)
+#     controls = WidgetBox(category_selection)
 
-    layout = row(controls, p)
-    # curdoc().add_root(layout)
+#     layout = row(controls, p)
+#     # curdoc().add_root(layout)
 
-    tab = Panel(child=layout, title='Category Wise Reviews')
-    return tab
+#     tab = Panel(child=layout, title='Category Wise Reviews')
+#     return tab
 
 
 def getKColors(k):
