6f58a7aa854d06ac89b1c59a7aad5c6ab953f12b
calvincodes <arpitjain1821@gmail.com>
2019-11-10 10:12:16 -0600

    Adding search functionality in product review over time

--- myapp/scripts/product_reviews_over_time.py
+++ myapp/scripts/product_reviews_over_time.py
@@ -8,7 +8,7 @@ from bokeh.io import show, output_notebook, push_notebook
 from bokeh.plotting import figure
 
 from bokeh.models import CategoricalColorMapper, HoverTool, ColumnDataSource, Panel, NumeralTickFormatter
-from bokeh.models.widgets import CheckboxGroup, Slider, RangeSlider, Tabs, RadioButtonGroup, Select
+from bokeh.models.widgets import CheckboxGroup, Slider, RangeSlider, Tabs, RadioButtonGroup, Button, TextInput, Div
 
 from bokeh.layouts import column, row, WidgetBox
 from bokeh.palettes import Category20_20
@@ -25,15 +25,19 @@ def product_reviews_over_time_tab(dataset, metadata):
     combined_data = dataset.set_index('asin').join(metadata.set_index('Product ID')).reset_index()
     combined_data.columns = ['asin', 'reviewerID', 'overall', 'unixReviewTime', 'Description', 'price', 'Category']
 
+    def get_product_data(product_id):
+        product_data = combined_data[combined_data['asin'] == product_id]
+
+        product_data['dtReviewTime'] = pd.to_datetime(product_data['unixReviewTime'], unit='s')
+        product_data['reviewYear'] = product_data['dtReviewTime'].dt.strftime('%Y')
+        product_data['reviewMonth'] = product_data['dtReviewTime'].dt.strftime('%Y-%m')
+        product_data['dtReviewTime'] = product_data['dtReviewTime'].dt.strftime('%Y-%m-%d')
+        product_data = product_data.sort_values('dtReviewTime')
+        return product_data
+
     # Default selected_product is the most_purchased_product
     selected_product = combined_data.asin.value_counts().head(1).index[0]
-    filtered_data = combined_data[combined_data['asin'] == selected_product]
-
-    filtered_data['dtReviewTime'] = pd.to_datetime(filtered_data['unixReviewTime'], unit='s')
-    filtered_data['reviewYear'] = filtered_data['dtReviewTime'].dt.strftime('%Y')
-    filtered_data['reviewMonth'] = filtered_data['dtReviewTime'].dt.strftime('%Y-%m')
-    filtered_data['dtReviewTime'] = filtered_data['dtReviewTime'].dt.strftime('%Y-%m-%d')
-    filtered_data = filtered_data.sort_values('dtReviewTime')
+    filtered_data = get_product_data(selected_product)
 
     year_wise_reviews = filtered_data.groupby('reviewYear')['overall'].agg(['mean', 'count']).reset_index()
     year_wise_reviews.columns = ['time', 'average', 'total']
@@ -100,7 +104,31 @@ def product_reviews_over_time_tab(dataset, metadata):
     radio_button_group = RadioButtonGroup(
         labels=["Yearly", "Monthly", "Daily"], active=0)
 
+    def get_updated_plot_data_dict(new_plot_data):
+
+        new_data = dict()
+
+        if new_plot_data.empty:
+            new_data['x_range'] = []
+            new_data['time_stamp'] = []
+            new_data['average'] = []
+            new_data['total'] = []
+            new_data['color'] = []
+
+        else:
+            new_colors = getKColors(len(new_plot_data))
+
+            new_data['x_range'] = new_plot_data.time.tolist()
+            new_data['time_stamp'] = new_plot_data.time.tolist()
+            new_data['average'] = new_plot_data.average.tolist()
+            new_data['total'] = new_plot_data.total.tolist()
+            new_data['color'] = new_colors
+
+        return new_data
+
     def update_plot(attr, old, new):
+
+        global year_wise_reviews, month_wise_reviews, date_wise_reviews
 
         new_plot_data = plot_data
 
@@ -111,14 +139,7 @@ def product_reviews_over_time_tab(dataset, metadata):
         if radio_button_group.active == 2:
             new_plot_data = date_wise_reviews
 
-        new_colors = getKColors(len(new_plot_data))
-
-        new_data = dict()
-        new_data['x_range'] = new_plot_data.time.tolist()
-        new_data['time_stamp'] = new_plot_data.time.tolist()
-        new_data['average'] = new_plot_data.average.tolist()
-        new_data['total'] = new_plot_data.total.tolist()
-        new_data['color'] = new_colors
+        new_data = get_updated_plot_data_dict(new_plot_data)
 
         p1.x_range.factors = new_data['x_range']
         p2.x_range.factors = new_data['x_range']
@@ -128,14 +149,60 @@ def product_reviews_over_time_tab(dataset, metadata):
 
     radio_button_group.on_change('active', update_plot)
 
-    def update_selection(attr, old, new):
-        #TODO: Implement this
-        return
+    product_details_div = Div(text="""<b>This is a sample div.</b>""", width=200, height=100)
 
-    # select_product = Select(title="Product Id:", value=selected_product, options=combined_data['asin'].unique().tolist())
-    # select_product.on_change('value', update_selection)
+    def update_selection():
 
-    # layout = column(select_product, radio_button_group, p1, p2)
-    layout = column(radio_button_group, p1, p2)
+        global year_wise_reviews, month_wise_reviews, date_wise_reviews
+        searched_data = get_product_data(search_input.value)
+        new_data = dict()
+
+        if searched_data.empty:
+
+            year_wise_reviews = searched_data
+            month_wise_reviews = searched_data
+            date_wise_reviews = searched_data
+
+            new_data['x_range'] = []
+            new_data['time_stamp'] = []
+            new_data['average'] = []
+            new_data['total'] = []
+            new_data['color'] = []
+
+            p1.x_range.factors = new_data['x_range']
+            p2.x_range.factors = new_data['x_range']
+
+            product_details_div.text = """<b>Please get updated.</b>"""
+
+        else:
+            year_wise_reviews = searched_data.groupby('reviewYear')['overall'].agg(['mean', 'count']).reset_index()
+            year_wise_reviews.columns = ['time', 'average', 'total']
+
+            month_wise_reviews = searched_data.groupby('reviewMonth')['overall'].agg(['mean', 'count']).reset_index()
+            month_wise_reviews.columns = ['time', 'average', 'total']
+
+            date_wise_reviews = searched_data.groupby('dtReviewTime')['overall'].agg(['mean', 'count']).reset_index()
+            date_wise_reviews.columns = ['time', 'average', 'total']
+
+            if radio_button_group.active == 0:
+                new_plot_data = year_wise_reviews
+            if radio_button_group.active == 1:
+                new_plot_data = month_wise_reviews
+            if radio_button_group.active == 2:
+                new_plot_data = date_wise_reviews
+
+            new_data = get_updated_plot_data_dict(new_plot_data)
+
+            p1.x_range.factors = new_data['x_range']
+            p2.x_range.factors = new_data['x_range']
+
+        ds1.data = new_data
+        ds2.data = new_data
+
+    search_input = TextInput(value=selected_product, title="Product ID:")
+    search_button = Button(label="Search", button_type="success")
+    search_button.on_click(update_selection)
+
+    layout = column(search_input, search_button, radio_button_group, product_details_div, p1, p2)
     tab = Panel(child=layout, title='Product Reviews Over Time')
     return tab
